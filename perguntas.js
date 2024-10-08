67edcd092b81a2c006bf18553f7330091a60a595
gasparim <143743544+gasparim10efaixa@users.noreply.github.com>
2024-10-08 11:46:57 -0300

    erterhet

---
+++ perguntas.js
@@ -0,0 +1,102 @@
+criaCartao(
+    'Matemática',
+    'Como se calcula a soma dos termos de uma P.A?',
+    'Utiliza-se a fórmula: ((a1+an).q)/2'
+)
+
+//20 perguntas ao todo
+criaCartao(
+    'Matemática',
+    'Como se calcula a soma dos termos de uma P.A?',
+    'Utiliza-se a fórmula: ((a1+an).q)/2'
+)
+criaCartao(
+    'Matemática',
+    'Como se calcula a soma dos termos de uma P.A?',
+    'Utiliza-se a fórmula: ((a1+an).q)/2'
+)
+criaCartao(
+    'Matemática',
+    'Como se calcula a soma dos termos de uma P.A?',
+    'Utiliza-se a fórmula: ((a1+an).q)/2'
+)
+criaCartao(
+    'Matemática',
+    'Como se calcula a soma dos termos de uma P.A?',
+    'Utiliza-se a fórmula: ((a1+an).q)/2'
+)
+criaCartao(
+    'Matemática',
+    'Como se calcula a soma dos termos de uma P.A?',
+    'Utiliza-se a fórmula: ((a1+an).q)/2'
+)
+criaCartao(
+    'Matemática',
+    'Como se calcula a soma dos termos de uma P.A?',
+    'Utiliza-se a fórmula: ((a1+an).q)/2'
+)
+criaCartao(
+    'Matemática',
+    'Como se calcula a soma dos termos de uma P.A?',
+    'Utiliza-se a fórmula: ((a1+an).q)/2'
+)
+criaCartao(
+    'Matemática',
+    'Como se calcula a soma dos termos de uma P.A?',
+    'Utiliza-se a fórmula: ((a1+an).q)/2'
+)
+criaCartao(
+    'Matemática',
+    'Como se calcula a soma dos termos de uma P.A?',
+    'Utiliza-se a fórmula: ((a1+an).q)/2'
+)
+criaCartao(
+    'Matemática',
+    'Como se calcula a soma dos termos de uma P.A?',
+    'Utiliza-se a fórmula: ((a1+an).q)/2'
+)
+criaCartao(
+    'Matemática',
+    'Como se calcula a soma dos termos de uma P.A?',
+    'Utiliza-se a fórmula: ((a1+an).q)/2'
+)
+criaCartao(
+    'Matemática',
+    'Como se calcula a soma dos termos de uma P.A?',
+    'Utiliza-se a fórmula: ((a1+an).q)/2'
+)
+criaCartao(
+    'Matemática',
+    'Como se calcula a soma dos termos de uma P.A?',
+    'Utiliza-se a fórmula: ((a1+an).q)/2'
+)
+criaCartao(
+    'Matemática',
+    'Como se calcula a soma dos termos de uma P.A?',
+    'Utiliza-se a fórmula: ((a1+an).q)/2'
+)
+criaCartao(
+    'Matemática',
+    'Como se calcula a soma dos termos de uma P.A?',
+    'Utiliza-se a fórmula: ((a1+an).q)/2'
+)
+criaCartao(
+    'Matemática',
+    'Como se calcula a soma dos termos de uma P.A?',
+    'Utiliza-se a fórmula: ((a1+an).q)/2'
+)
+criaCartao(
+    'Matemática',
+    'Como se calcula a soma dos termos de uma P.A?',
+    'Utiliza-se a fórmula: ((a1+an).q)/2'
+)
+criaCartao(
+    'Matemática',
+    'Como se calcula a soma dos termos de uma P.A?',
+    'Utiliza-se a fórmula: ((a1+an).q)/2'
+)
+criaCartao(
+    'Matemática',
+    'Como se calcula a soma dos termos de uma P.A?',
+    'Utiliza-se a fórmula: ((a1+an).q)/2'
+)

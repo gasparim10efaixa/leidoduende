409bc3c2443fba34a81b69320c482ba5e5791591
gasparim <143743544+gasparim10efaixa@users.noreply.github.com>
2024-10-08 12:23:31 -0300

    lsdmgfkoergnfigvnikvnçv

--- perguntas.js
+++ perguntas.js
@@ -1,102 +1,102 @@
 criaCartao(
-    'Matemática',
-    'Como se calcula a soma dos termos de uma P.A?',
-    'Utiliza-se a fórmula: ((a1+an).q)/2'
+    'Ed. física',
+    'Qual o esporte mais popular do mundo',
+    'futebol'
 )
 
 //20 perguntas ao todo
 criaCartao(
-    'Matemática',
-    'Como se calcula a soma dos termos de uma P.A?',
-    'Utiliza-se a fórmula: ((a1+an).q)/2'
+    'Ed. Física',
+    'Qual o esporte mais popular nos EUA',
+    'Futebol Americano'
 )
 criaCartao(
     'Matemática',
-    'Como se calcula a soma dos termos de uma P.A?',
-    'Utiliza-se a fórmula: ((a1+an).q)/2'
+    'Se x é igual a 23, quanto vale 3x?',
+    '69'
 )
 criaCartao(
     'Matemática',
-    'Como se calcula a soma dos termos de uma P.A?',
-    'Utiliza-se a fórmula: ((a1+an).q)/2'
+    'Qual é o valor do perímetro do quadrado de lado 4cm?',
+    '16 cm'
+)
+criaCartao(
+    'Português',
+    'quais os tempos da língua portuguesa?',
+    'Passado, presente e futuro'
+)
+criaCartao(
+    'Biologia',
+    'Qual é a porcentagem de nascer um filhote homozigoto dominante?',
+    '25 porcento'
+)
+criaCartao(
+    'Geografia',
+    'Qual é a capital do Uruguai',
+    'montevidéu'
+)
+criaCartao(
+    'Geografia',
+    'Qual é a capital da Colômbia?',
+    'bogotá'
+)
+criaCartao(
+    'Geografia',
+    'Qual é a capital do Brasil?',
+    'brasília'
+)
+criaCartao(
+    'Geografia',
+    'Qual continente mora  o Neymar',
+    'Ásia'
 )
 criaCartao(
     'Matemática',
-    'Como se calcula a soma dos termos de uma P.A?',
-    'Utiliza-se a fórmula: ((a1+an).q)/2'
+    'qual o primeiro numero primo',
+    '2'
 )
 criaCartao(
     'Matemática',
-    'Como se calcula a soma dos termos de uma P.A?',
-    'Utiliza-se a fórmula: ((a1+an).q)/2'
+    'como calcula o perímetro do triângulo',
+    'some todos os lados'
 )
 criaCartao(
-    'Matemática',
-    'Como se calcula a soma dos termos de uma P.A?',
-    'Utiliza-se a fórmula: ((a1+an).q)/2'
+    'Inglês',
+    'Como se diz oi em inglês',
+    'hello'
 )
 criaCartao(
-    'Matemática',
-    'Como se calcula a soma dos termos de uma P.A?',
-    'Utiliza-se a fórmula: ((a1+an).q)/2'
+    'Inglês',
+    'Como se diz tchau em inglês?',
+    'bye'
 )
 criaCartao(
-    'Matemática',
-    'Como se calcula a soma dos termos de uma P.A?',
-    'Utiliza-se a fórmula: ((a1+an).q)/2'
+    'Inglês',
+    'O que é bed em português?',
+    'Cama'
 )
 criaCartao(
-    'Matemática',
-    'Como se calcula a soma dos termos de uma P.A?',
-    'Utiliza-se a fórmula: ((a1+an).q)/2'
+    'Inglês',
+    'Como se fala feliz em inglês?',
+    'Happy'
 )
 criaCartao(
-    'Matemática',
-    'Como se calcula a soma dos termos de uma P.A?',
-    'Utiliza-se a fórmula: ((a1+an).q)/2'
+    'Inglês',
+    'Como se diz eu te amo em inglês?',
+    'I love you'
 )
 criaCartao(
-    'Matemática',
-    'Como se calcula a soma dos termos de uma P.A?',
-    'Utiliza-se a fórmula: ((a1+an).q)/2'
+    'Inglês',
+    'Como se diz cadeira em inglês?',
+    'Chair'
 )
 criaCartao(
-    'Matemática',
-    'Como se calcula a soma dos termos de uma P.A?',
-    'Utiliza-se a fórmula: ((a1+an).q)/2'
+    'Inglês',
+    'Como se fala mesa em inglês',
+    'Desk/Table'
 )
 criaCartao(
-    'Matemática',
-    'Como se calcula a soma dos termos de uma P.A?',
-    'Utiliza-se a fórmula: ((a1+an).q)/2'
-)
-criaCartao(
-    'Matemática',
-    'Como se calcula a soma dos termos de uma P.A?',
-    'Utiliza-se a fórmula: ((a1+an).q)/2'
-)
-criaCartao(
-    'Matemática',
-    'Como se calcula a soma dos termos de uma P.A?',
-    'Utiliza-se a fórmula: ((a1+an).q)/2'
-)
-criaCartao(
-    'Matemática',
-    'Como se calcula a soma dos termos de uma P.A?',
-    'Utiliza-se a fórmula: ((a1+an).q)/2'
-)
-criaCartao(
-    'Matemática',
-    'Como se calcula a soma dos termos de uma P.A?',
-    'Utiliza-se a fórmula: ((a1+an).q)/2'
-)
-criaCartao(
-    'Matemática',
-    'Como se calcula a soma dos termos de uma P.A?',
-    'Utiliza-se a fórmula: ((a1+an).q)/2'
-)
-criaCartao(
-    'Matemática',
-    'Como se calcula a soma dos termos de uma P.A?',
-    'Utiliza-se a fórmula: ((a1+an).q)/2'
+    'Inglês',
+    'Como se diz professor em inglês?',
+    'Teacher'
 )

d3c46ebd0e9395ee1eacf223fcc1b139647b8a41
gasparim <143743544+gasparim10efaixa@users.noreply.github.com>
2024-10-15 11:18:03 -0300

    hhthhh

--- main.js
+++ main.js
@@ -2,5 +2,7 @@ function criaCartao (categoria,pergunta,resposta){
 let container = document.getElementById('container');
 let cartao = document.createElement('article');
 cartao.className = 'cartao';
+cartao.innerHTML = `
 
+`
 }
